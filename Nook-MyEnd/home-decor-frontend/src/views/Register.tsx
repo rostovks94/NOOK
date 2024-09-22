8025780507bbb4210761c023cac445f958e4cb0c
Kseniia Rostovskaia <165850515+rostovks94@users.noreply.github.com>
2024-09-22 16:57:35 -0400

    Update Register.tsx

--- Nook-MyEnd/home-decor-frontend/src/views/Register.tsx
+++ Nook-MyEnd/home-decor-frontend/src/views/Register.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth'; 
-import { auth } from '../../firebaseConfig'; 
+import { createUserWithEmailAndPassword } from 'firebase/auth'; 
+import { auth } from '../firebaseConfig'; 
 import '../css/Register.css';
 import logoImage from '../assets/NookLogo.png'; 
 
 const Register: React.FC = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [isValidEmail, setIsValidEmail] = useState(false);
   const [error, setError] = useState('');
 
@@ -20,17 +19,13 @@ const Register: React.FC = () => {
     setIsValidEmail(emailValidationRegex.test(emailValue));
   };
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
   const handleFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     if (isValidEmail) {
       try {
-        // Use createUserWithEmailAndPassword for registration with email and password
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        // Use createUserWithEmailAndPassword for registration with email
+        const userCredential = await createUserWithEmailAndPassword(auth, email, ''); // No password provided
         const token = await userCredential.user.getIdToken();
         localStorage.setItem('authToken', token);
         navigate('/complete-signup');
@@ -44,23 +39,6 @@ const Register: React.FC = () => {
       }
     } else {
       setError('Please enter a valid email.');
-    }
-  };
-
-  const handleGoogleSignUp = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const token = await result.user.getIdToken();
-      localStorage.setItem('authToken', token);
-      navigate('/mainfeed');
-    } catch (error: any) {
-      if (error.code === 'auth/email-already-in-use') {
-        setError('This email is already registered with Google. Please sign in to your account.');
-      } else {
-        setError('Google sign-up error. Please try again.');
-      }
-      console.error('Error during Google Sign-Up:', error);
     }
   };
 
@@ -80,22 +58,9 @@ const Register: React.FC = () => {
               required 
             />
           </div>
-          <div className="register-input-container">
-            <label>Password</label>
-            <input 
-              type="password" 
-              placeholder="Enter Password" 
-              value={password}
-              onChange={handlePasswordChange}
-              required 
-            />
-          </div>
           {error && <p className="error-message">{error}</p>}
           <button type="submit" className="register-create-account-button">Sign Up</button>
-          <div className="register-divider"><span>OR</span></div>
-          <button type="button" className="register-google-signup-button" onClick={handleGoogleSignUp}>
-            Sign up with Google
-          </button>
+          <div className="register-divider"><span>OR</span></div> {/* Разделитель OR */}
           <button type="button" className="register-login-button" onClick={() => navigate('/login')}>Sign In</button>
         </form>
       </div>
